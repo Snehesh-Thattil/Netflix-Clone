64454677eb8ad695bdbbd2c1f28c5a66a6e6aa8b
Snehesh-Thattil <sneheshthattil75@gmail.com>
2024-09-01 16:39:07 +0530

    [ part one with design elements findished , Navbar yet to be finished ]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './Components/Banner/Banner';
 import Navbar from './Components/Navbar/Navbar';
+import Trending from './Components/Trending/Trending';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <Trending />
     </div>
   )
 }
