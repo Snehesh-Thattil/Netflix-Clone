6fca41c71e7bae38327e5e554275f8018c1cd577
Snehesh-Thattil <sneheshthattil75@gmail.com>
2024-09-01 13:32:03 +0530

    [Initial Commit with partial navbar, banner, and bottom fade effect]

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className='banner'>
+            <div className="content">
+                <h1 className='title'>Money Heist</h1>
+                <div className="buttons">
+                    <button>Play</button>
+                    <button>List</button>
+                </div>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos. amet consectetur adipisicing elit. Dignissimos nihil nam velit sed. Accusamus modi dolores consectetur voluptatem dolorem quam a, nulla unde iure at autem culpa numquam? Illum, nostrum.</p>
+            </div>
+            <div className="fade-bottom"></div>
+        </div>
+    )
+}
+
+export default Banner
