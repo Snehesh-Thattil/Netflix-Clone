99e8f3eda037c8abeef5f84730715715623f397e
Snehesh Thattil <sneheshthattil75@gmail.com>
2025-02-10 17:25:48 +0530

    [ Login page introduced with design, not functional yet : 13 ]

--- src/App.js
+++ src/App.js
@@ -6,20 +6,32 @@ import MyList from './Components/NavItems/MyList'
 import NewsAndPopular from './Components/NavItems/NewsAndPopular'
 import BrowseByLanguage from './Components/NavItems/BrowseByLanguage'
 import Home from './Home';
+import { useEffect, useState } from 'react';
+import Login from './Components/Login/Login';
 
 function App() {
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    let userFetch = null
+    setUser(userFetch)
+  }, [])
+
   // Render
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route exact element={<Home />} path='/'></Route>
-          <Route element={<Movies />} path='/movies'></Route>
-          <Route element={<TVshows />} path='/tvshows'></Route>
-          <Route element={<MyList />} path='/mylist'></Route>
-          <Route element={<NewsAndPopular />} path='/news-and-popular'></Route>
-          <Route element={<BrowseByLanguage />} path='/browsebylanguage'></Route>
-        </Routes>
+        {!user ? <Login />
+          :
+          <Routes>
+            <Route exact element={<Home />} path='/'></Route>
+            <Route element={<Movies />} path='/movies'></Route>
+            <Route element={<TVshows />} path='/tvshows'></Route>
+            <Route element={<MyList />} path='/mylist'></Route>
+            <Route element={<NewsAndPopular />} path='/news-and-popular'></Route>
+            <Route element={<BrowseByLanguage />} path='/browsebylanguage'></Route>
+          </Routes>
+        }
       </Router>
     </div>
   )
